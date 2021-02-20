fb54221da1261ac464607db5ba0737f94b04275c
FIZLIN <akostov94@gmail.com>
2021-02-20 10:44:12 +0200

    Added exam 2 - revisited

---
+++ exam-2-revisited/api/resources/user.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res, next) => {
+  //...
+});
+router.get('/:id', (req, res, next) => {
+  //...
+});
+
+module.exports = router;
